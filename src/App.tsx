f45a3bfcb3624ddf4a13141c2fe55561c7dd1146
pedroamarop <pedro.a.p.dev@gmail.com>
2026-08-13 09:55:56 -0300

    feat: implementa página "Quem Somos" com conteúdo sobre a equipe e missão da MoovUp

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Layout from './components/layout/Layout.tsx';
 import Home from './pages/Home.tsx';
 // import Sobre from './pages/Sobre.tsx';
 // import Solucao from './pages/Solucao.tsx';
-// import QuemSomos from './pages/QuemSomos.tsx';
+import QuemSomos from './pages/QuemSomos.tsx';
 // import Faq from './pages/Faq.tsx';
 // import Contato from './pages/Contato.tsx';
 
@@ -12,11 +12,11 @@ function App() {
         <Routes>
             <Route element={<Layout />}>
                 <Route index element={<Home />} /> 
-                {/* <Route path="quem-somos" element={<QuemSomos />} />
-                <Route path="sobre" element={<Sobre />} />
-                <Route path="solucao" element={<Solucao />} />
-                <Route path="faq" element={<Faq />} />
-                <Route path="contato" element={<Contato />} /> */}
+                <Route path="quem-somos" element={<QuemSomos />}/>
+                {/* <Route path="sobre" element={<Sobre />} /> */}
+                {/* <Route path="solucao" element={<Solucao />} /> */}
+                {/* <Route path="faq" element={<Faq />} /> */}
+                {/* <Route path="contato" element={<Contato />} /> */}
             </Route>
         </Routes>
     )
